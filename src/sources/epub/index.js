3c07aae0395a829ac1d8c9341476b5d9e0128dd9
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-06-03 23:57:20 +0200

    ePub

--- src/sources/epub/index.js
+++ src/sources/epub/index.js
@@ -8,15 +8,58 @@ module.exports = (filename) => {
 			return new Promise((resolve) => {
 				book._epub = new EPub(filename);
 
-				book.epub.on("end", function(){
-					resolve();
+				book._epub.on("end", function(){
+					resolve(book);
 				});
 
 				book._epub.parse();
+			});
+		},
+		_getChapter: (id) => {
+			return new Promise((resolve, reject) => {
+				book._epub.getChapter(id, (error, content) => {
+					if(error){
+						reject(error);
+					}
+
+					resolve(content);
+				});
+			});
+		},
+
+		getTitle: () => {
+			return book._epub.metadata.title;
+		},
+		getChapters: () => {
+			return new Promise((resolve) => {
+				let chaptersContent = [];
+
+				let chapters = book._epub.flow.map(function(chapter){
+					let chapterResult = {
+						id: chapter.id,
+						title: chapter.title 
+					};
+
+					chaptersContent.push(book._getChapter(chapter.id));
+
+					return chapterResult;
+				});
+
+				Promise.all(chaptersContent).then((content) => {
+					content.forEach((content, key) => {
+						chapters[key].content = content;
+					});
+
+					console.log(chapters);
+				});
 			});
 		}
 	};
 
 	return book._init(filename);
 };
+
+module.exports("./books/Metamorphosis-jackson.epub").then((book) => {
+	book.getChapters();
+});
 
