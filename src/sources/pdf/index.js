e328bcccc5cdbac2d8f385e9019472485b9df81d
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-06-04 00:23:46 +0200

    Book engine tests

--- src/sources/pdf/index.js
+++ src/sources/pdf/index.js
@@ -1,4 +1,16 @@
 module.exports = (filename) => {
+	const book = {
+		_init: (filename) => {
+			return new Promise((resolve) => {
+				resolve(book);
+			});
+		},
 
+		getTitle: () => {
+			return "";
+		}
+	};
+
+	return book._init(filename);
 };
 
--- tests/books/index.js
+++ tests/books/index.js
@@ -3,18 +3,26 @@
 const expect = require("chai").expect;
 
 const epub = require("../../src/sources/epub");
-const pdf = require("./../src/sources/pdf");
+const pdf = require("../../src/sources/pdf");
 
 const validateBookFormat = (engine, file) => {
-	engine(file).then((book) => {
+	let promise = engine(file)
+
+	expect(promise).to.be.a("promise");
+
+	promise.then((book) => {
+		expect(book.getTitle).to.be.a("function");
+
 		expect(book.getTitle()).to.be.a("string");
 
-		book.getChapters().then((chapters) => {
-			expect(chapters.length).to.equal(8);
+		expect(book.getChapters).to.be.a("function");
 
+		book.getChapters().then((chapters) => {
 			expect(chapters).to.be.a("array");
 
 			chapters.forEach((chapter) => {
+				expect(chapter).to.be.a("object");
+
 				expect(chapter.title).to.be.a("string");
 				expect(chapter.content).to.be.a("string");
 			});
@@ -27,7 +35,10 @@ describe("Book engines", function() {
 		it("Decodes ePub book into uniread format", function() {
 			validateBookFormat(epub, "./books/Metamorphosis-jackson.epub");
 		});
+	});
 
+
+	describe("pdf book engine", function() {
 		it("Decodes pdf book into uniread format", function() {
 			validateBookFormat(pdf, "./books/Metamorphosis-jackson.pdf");
 		});
