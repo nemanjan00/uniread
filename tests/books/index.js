4e09821291d81dd7d70116fe969812cb0fe9e20a
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-06-04 00:15:56 +0200

    pdf

--- tests/books/index.js
+++ tests/books/index.js
@@ -2,14 +2,17 @@
 
 const expect = require("chai").expect;
 
-const epub = require("../../src/sources/epub/index.js");
+const epub = require("../../src/sources/epub");
+const pdf = require("./../src/sources/pdf");
 
-const validateBookFormat = (engine) => {
-	engine("./books/Metamorphosis-jackson.epub").then((book) => {
-		expect(book.getTitle()).to.equal("Metamorphosis");
+const validateBookFormat = (engine, file) => {
+	engine(file).then((book) => {
+		expect(book.getTitle()).to.be.a("string");
 
 		book.getChapters().then((chapters) => {
 			expect(chapters.length).to.equal(8);
+
+			expect(chapters).to.be.a("array");
 
 			chapters.forEach((chapter) => {
 				expect(chapter.title).to.be.a("string");
@@ -22,7 +25,11 @@ const validateBookFormat = (engine) => {
 describe("Book engines", function() {
 	describe("ePub book engine", function() {
 		it("Decodes ePub book into uniread format", function() {
-			validateBookFormat(epub);
+			validateBookFormat(epub, "./books/Metamorphosis-jackson.epub");
+		});
+
+		it("Decodes pdf book into uniread format", function() {
+			validateBookFormat(pdf, "./books/Metamorphosis-jackson.pdf");
 		});
 	});
 });
