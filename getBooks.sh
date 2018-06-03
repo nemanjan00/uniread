95a4f2f44a5087ea085dcea863d5a111d2af5837
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-06-03 15:53:29 +0200

    Get books better

--- getBooks.sh
+++ getBooks.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
+pushd $(git rev-parse --show-toplevel)
+
+echo "[*] Creating books folder in the root of project"
+
+mkdir -p ./books
+
 echo "[*] Downloading sample books for testing"
 
-wget --directory-prefix=./books https://s3-us-west-2.amazonaws.com/pressbooks-samplefiles/MetamorphosisJacksonTheme/Metamorphosis-jackson.pdf
-wget --directory-prefix=./books https://s3-us-west-2.amazonaws.com/pressbooks-samplefiles/MetamorphosisJacksonTheme/Metamorphosis-jackson.epub
-wget --directory-prefix=./books https://s3-us-west-2.amazonaws.com/pressbooks-samplefiles/MetamorphosisJacksonTheme/Metamorphosis-jackson.mobi
+if [ -f ./books/Metamorphosis-jackson.pdf ]; then
+	echo "[-] ./books/Metamorphosis-jackson.pdf already exists"
+else
+	wget --directory-prefix=./books https://s3-us-west-2.amazonaws.com/pressbooks-samplefiles/MetamorphosisJacksonTheme/Metamorphosis-jackson.pdf
+fi
+
+if [ -f ./books/Metamorphosis-jackson.mobi ]; then
+	echo "[-] ./books/Metamorphosis-jackson.mobi already exists"
+else
+	wget --directory-prefix=./books https://s3-us-west-2.amazonaws.com/pressbooks-samplefiles/MetamorphosisJacksonTheme/Metamorphosis-jackson.mobi
+fi
+
+if [ -f ./books/Metamorphosis-jackson.epub ]; then
+	echo "[-] ./books/Metamorphosis-jackson.epub already exists"
+else
+	wget --directory-prefix=./books https://s3-us-west-2.amazonaws.com/pressbooks-samplefiles/MetamorphosisJacksonTheme/Metamorphosis-jackson.epub
+fi
+
+popd
+
