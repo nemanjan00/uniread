fcb5b880a26ef24d4ae02f57512f8672d50e7b86
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-06-07 21:51:51 +0200

    Check for update

--- bin/uniread.js
+++ bin/uniread.js
@@ -1,10 +1,28 @@
 #!/usr/bin/env node
 
+const updateNotifier = require("update-notifier");
+const pkg = require("../package.json");
+
 const uniread = require("../").uniread;
 const spritz = require("../").spritz;
 
-uniread.getBook(process.argv[process.argv.length - 1]).then((book) => {
-	spritz(book);
-});
+const run = () => {
+	uniread.getBook(process.argv[process.argv.length - 1]).then((book) => {
+		spritz(book);
+	});
+};
 
+const notifier = updateNotifier({pkg: pkg, defer: false, callback: (error, response) => {
+	if(error){
+		run();
+	}
+
+	if(response.type == "latest"){
+		run();
+	} else {
+		setTimeout(run, 2000);
+	}
+}});
+
+notifier.notify();
 
