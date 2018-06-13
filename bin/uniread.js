2b3419b9dda377c643dbdddb5734bc6783eead68
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-06-13 12:19:45 +0200

    Timeout

--- bin/uniread.js
+++ bin/uniread.js
@@ -6,27 +6,39 @@ const pkg = require("../package.json");
 const uniread = require("../").uniread;
 const spritz = require("../").spritz;
 
+let timeout = false;
+
 const run = () => {
 	uniread.getBook(process.argv[process.argv.length - 1]).then((book) => {
 		spritz(book);
 	});
 };
 
+setTimeout(() => {
+	if(!timeout){
+		timeout = true;
+		run();
+	}
+}, 2000);
+
 const notifier = updateNotifier({
 	pkg: pkg,
 	callback: (error, response) => {
-		if(error){
-			run();
-		}
+		if(!timeout){
+			timeout = true;
+			if(error){
+				run();
+			}
 
-		if(response.type == "latest"){
-			run();
-		} else {
-			notifier.update = response;
+			if(response.type == "latest"){
+				run();
+			} else {
+				notifier.update = response;
 
-			notifier.notify({defer: false, isGlobal: true});
+				notifier.notify({defer: false, isGlobal: true});
 
-			setTimeout(run, 2000);
+				setTimeout(run, 2000);
+			}
 		}
 	}
 });
